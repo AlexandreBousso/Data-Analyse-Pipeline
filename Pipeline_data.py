83a927c4e7b93ee9fa78c0789d44ae5c6b436596
AlexandreBousso <alexandre.boussoffara@yahoo.fr>
2026-02-19 18:21:39 +0100

    Add files via upload

--- Pipeline_data.py
+++ Pipeline_data.py
@@ -29,6 +29,44 @@ def load_API(url, API_KEY):
     else:
         print(f"Erreur lors de la requête API, Code d'erreur : {response.status_code}") 
         return None
+
+def load_database(source, api_key=None): #Je rassemble mes fonctions load_path et load_API dans une seule fonction afin de pouvoir automatiser plus simplement mon pipeline
+    if source.startswith(("http://", "https://")):
+        if api_key is None:
+            print("Clé API requise")
+            return(None)
+        headers = {"Authorization":f'Bearer {api_key}'}
+        try:
+            response = requests.get(source, headers=headers, timeout=10)
+            if response.status_code == 200:
+                data = response.json() 
+                return pd.DataFrame(data)
+            else:
+                print(f"Erreur lors de la requête API, Code d'erreur : {response.status_code}") 
+                return None
+        except Exception as e:
+            print(f"Une erreur s'est produite lors de la requête API: {e}")
+            return None
+    else:
+        if not os.path.exists(source):
+            print(f"Le fichier '{source}' n'existe pas.")
+            return None
+        extension = os.path.splitext(source)[1].lower() #permet d'extraire l'extension du fichier et mettre en minuscule. 
+        try:
+            if extension ==".csv":
+                return pd.read_csv(source, sep=",")
+            elif extension in [".xls", ".xlsx"]:
+                return pd.read_excel(source)
+            else:
+                print("Format de fichier incompatible")
+                return(None)
+        except Exception as e:
+            print(f"Une erreur s'est produite lors du chargement du fichier : {e}")
+            return None
+
+    
+
+
 def df_info(df):
     print("Aperçu des données :")
     print(df.head())
@@ -40,10 +78,18 @@ def df_info(df):
 def check_missing(df):
     print("Analyse des valeurs manquantes :")
     missing = df.isnull().sum()
+    summary = missing.reset_index()
+    summary.columns = ["Colonne", "Nb manquants"]
+    manquant= summary[summary['Nb manquants'] > 0]
     missing_percent = 100 * df.isnull().sum() / len(df)
-    summary = pd.DataFrame({"Valeurs manquantes :": missing, "Pourcentage (%)": missing_percent})
-    print(summary[summary['Nb manquants'] > 0])
-    return(summary)
+    if manquant.empty:
+        print("Aucune valeur manquante détectée.")
+    else:
+        print("Valeurs manquantes :")
+        print(manquant)
+        print(missing_percent)
+    
+    
     return(df)
    
 
@@ -65,10 +111,25 @@ def replace_mapping(df, column, mapping):  #Fonction qui permet de remplacer les
     df[column] = df[column].map(mapping)
     return df
 
+
+#Fonction combo de mapping et replace values en fonction des besoins dans un soucis de faire un pipeline automatisé
+#keep_others=True pour utiliser .replace 
+#keep_others=False pour utiliser .map
+def transform_value(df, column, mapping, keep_others=True):
+    df=df.copy()
+    if keep_others:
+        print(f"Remplacement partiel dans {column} avec .replace")
+        df[column] = df[column].replace(mapping)
+    else:
+        print(f"Remplacement total dans {column} avec .map")
+        df[column] = df[column].map(mapping)
+    return df
+
 def check_missing_after_mapping(df, column): #fonction qui vérifie s'il y'a des valeurs manquantes après le mapping, à utiliser juste après (replace_mapping)
     missing = df[column].isna().sum()
     if missing > 0:
         print(f"⚠️ {missing} valeurs manquantes dans {column}")
+    return(df)
 
 def column_rename(df,old_name, new_name):
     df=df.copy()
@@ -80,29 +141,89 @@ def filter_rows(df, conditions):        #Avec conditions un dictionnaire du type
         df = df[df[column] == value]
     return df
 
+def convert_dtypes(df, dtype_map): #dtype map un dictionnaire tel que {"column1":dtype1, "column2"::dtype2 etc} avec dtype, int, float, str & datetime
+    df= df.copy()
+    for column, dtype in dtype_map.items():
+        try:
+            if dtype == "datetime":
+                df[column] = pd.to_datetime(df[column], errors='coerce')
+            else:
+                df[column]= df[column].astype(dtype)
+            print(f'Conversion de {column} en {dtype} réussie"')
+        except Exception as e:
+            print(f"Erreur lors de la conversion de {column} en {dtype} : {e}")
+    return df
+
 def aggregate_mean (df, columns, conditions):  #Fonction qui permet d'agréger les données en calculant la moyenne des values de "columns" après avoir filtré les donnés selons "conditions" de type conditions ={column1=value1, etc}
     df= df.copy()
     for column, value in conditions.items():
         df = df[df[column] == value]
     return df.groupby(columns).mean().reset_index()
 
+#Fonction qui permet d'aréger les donnés à l'aide d'un filtre "conditions" tel que conditions ={column1=value1, etc}} 
+#grpby_columns : liste des colonnes sur lesquelles on veut grouper sous la forme ["colonne1, "colonne2," etc]
+#agg_logic : dictionnaire tel que {"colonne1":"mean", "ventes":"sum", etc...}
 
-def aggregate (df, columns, conditions):  #Fonction qui permet d'aréger les donnés à l'aide d'un filtre "conditions"
+def aggregate (df, grpby_columns, agg_logic, conditions=None):  
     df= df.copy()
-    for column, value in conditions.items():
-        df = df[df[column] == value]
-    return df.groupby(columns).reset_index()
+    if conditions is not None:
+        for column, value in conditions.items():
+            df = df[df[column] == value]
+    return df.groupby(grpby_columns).agg(agg_logic).reset_index()
 
 def saving_file(df, path, format): #Fonction qui permet de save un data frame au format "csv" ou "excel"
     if format =="csv":
-        df.to_csv(path, index=False)
+        df.to_csv(f"{path}.csv", index=False)
     elif format in ["xls", "xlsx"]:
-        df.to_excel(path, index=False)
+        df.to_excel(f"{path}.xlsx", index=False)
     else:
         print("Format de fichier incompatible, veuillez choisir entre 'csv' ou 'excel'")
+    return df
 
-#Assemblage sous forme de pipelines
+#Assemblage sous forme de pipelines TLDR des fonctions précédentes
+#1er pipeline : Chargement de la base de données et affichage des informations via df_info et check_missing puis supressions des lignes manquantes avec df_drop_NAN(avec demande utilisateur)
+#Dans l'idée j'aimerai ajouter un pipeline intermédiaire pour filtrer et renommer les colonnes avant de faire le mapping mais pas une priorité actuelle, je vais me concentrer sur les opérations clés pour l'instant
+#2nd pipeline : Mapping ou remplacement de valeurs dans une colonne en fonction de True(replace) ou False(map) et vérification des valeurs manquantes après mapping
+#3éme pipeline: aggrégation des données à l'aide de conditions de filtrage et sauvegarde du résuldat dans un fichier csv ou excel
+def pipeline_load_and_info(source, api_key=None):
+    df=load_database(source, api_key)
+    if df is None:
+        print("Echec du chargement de la base de données")
+        return(None)
+    return (df.pipe(df_info).pipe(check_missing).pipe(df_drop_NAN))
+def pipeline_mapping_and_check(df, column, mapping):
+    return(df.pipe(transform_value, column=column, mapping=mapping, keep_others=True).pipe(check_missing_after_mapping, column=column))
+def pipeline_powerbirdy(df, grpby_columns, agg_logic, conditions=None, path=None, format=None):
+    return(df.pipe(aggregate, grpby_columns=grpby_columns, agg_logic=agg_logic, conditions=conditions).pipe(saving_file, path=path, format=format))
 
-df=(load_path(path).pipe(df_info).pipe(check_missing))      # Pipeline de chargement et d'affichages des informations du dataframe ainsi que les valeurs manquantes
-def pipeline_load_and_info(path):
-    return (load_path(path).pipe(df_info).pipe(check_missing).pipe(df_drop_NAN))
+
+
+def create_test_data():
+    data = {
+        'Date': ['2023-01-01', '2023-01-02', 'invalide', '2023-01-04'],
+        'Ville': ['pariis', 'lyon', 'marseille', 'lyon'], # Erreur "pariis"
+        'Ventes': ['100', '200', '150', '300'], # En texte au lieu de nombre
+        'Statut': ['V', 'V', 'A', 'V'] # Codes à mapper : V=Validé, A=Annulé
+    }
+    df_test = pd.DataFrame(data)
+    df_test.to_csv("test_brut.csv", index=False)
+    print("✅ Fichier 'test_brut.csv' créé pour l'exercice.")
+
+
+
+mapping_ville = {"pariis": "paris"}
+mapping_statut = {"V":"Validé", "A":"Annulé"}
+data_type={"Date":"datetime", "Ventes":"float", "Ville":"str", "Statut":"str"}
+agg_logic = {"Ventes":"sum"}
+
+def run_full_test():
+    print("--- DÉMARRAGE DU TEST ---")
+    df_resultat= (load_database("test_brut.csv").pipe(df_info).pipe(check_missing).pipe(transform_value, column="Ville", mapping=mapping_ville, keep_others=True).pipe(transform_value, column="Statut",
+     mapping=mapping_statut, keep_others=False).pipe(check_missing_after_mapping, column="Statut").pipe(convert_dtypes, dtype_map=data_type).pipe(df_drop_NAN).pipe(aggregate, grpby_columns=["Ville"], agg_logic=agg_logic).pipe(saving_file, path="export_power_bi", format="csv"))
+
+    print("---Test terminé---")
+    return df_resultat
+if __name__=="__main__":
+    create_test_data()
+    resultat_final = run_full_test()
+    print(resultat_final)
